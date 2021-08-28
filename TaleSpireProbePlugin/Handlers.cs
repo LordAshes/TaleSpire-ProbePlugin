bc2cabb3806684bca46f8b3fffe2429f0b0a522b
Lord Ashes <webtable@gmail.com>
2021-08-28 11:53:08 -0400

    Version 1.0.0

---
+++ TaleSpireProbePlugin/Handlers.cs
@@ -0,0 +1,76 @@
+﻿using BepInEx;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class ProbePlugin : BaseUnityPlugin
+    {
+        /// <summary>
+        /// Handler for Stat Messaging subscribed messages.
+        /// </summary>
+        /// <param name="changes"></param>
+        public void HandleRequest(StatMessaging.Change[] changes)
+        {
+            foreach (StatMessaging.Change change in changes)
+            {
+                if(change.action!=StatMessaging.ChangeType.removed)
+                {
+                    if (change.value == "[Probe]")
+                    {
+                        // Get results
+                        UnityEngine.Debug.Log("Probe Plugin: Sending Probe Results");
+                        StatMessaging.SetInfo(change.cid, change.key, GetPluginList());
+                    }
+                    else
+                    {
+                        // Record results
+                        if (LocalClient.IsInGmMode)
+                        {
+                            UnityEngine.Debug.Log("[" + change.key.Substring(ProbePlugin.Guid.Length + 1) + "]\r\n" + change.value);
+                            StatMessaging.ClearInfo(change.cid, change.key);
+                        }
+                        else
+                        {
+                            UnityEngine.Debug.Log("Probe Plugin: Ignoring Probe Result - Not GM");
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for Radial Menu selections
+        /// </summary>
+        /// <param name="cid"></param>
+        private string GetPluginList()
+        {
+            string result = "";
+            foreach(string pluginDir in System.IO.Directory.EnumerateDirectories(BepInEx.Paths.PluginPath))
+            {
+                bool dllFound = false;
+                string name = pluginDir.Substring(pluginDir.LastIndexOf(System.IO.Path.DirectorySeparatorChar) + 1);
+                foreach (string plugin in System.IO.Directory.EnumerateFiles(pluginDir,"*.DLL*"))
+                {
+                    if (plugin.ToUpper().EndsWith(".DLL"))
+                    {
+                        // Active plugin
+                        result = result + name + " (" + FileVersionInfo.GetVersionInfo(plugin).FileVersion + ")\r\n";
+                    }
+                    else
+                    {
+                        // Deactivated plugin
+                    }
+                    dllFound = true;
+                    break;
+                }
+                if (!dllFound)
+                {
+                    // Plugin with content only
+                    result = result + name + " (No Plugin, Content Only)\r\n";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ TaleSpireProbePlugin/ProbePlugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+using UnityEngine;
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(LordAshes.StatMessaging.Guid)]
+    public partial class ProbePlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Probe Plug-In";             
+        public const string Guid = "org.lordashes.plugins.probe";
+        public const string Version = "1.0.0.0";                 
+
+        // Configuration
+        private ConfigEntry<KeyboardShortcut> triggerKey { get; set; }      // Sample configuration for triggering a plugin via keyboard
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            // Not required but good idea to log this state for troubleshooting purpose
+            UnityEngine.Debug.Log("Probe Plugin: Active.");
+
+            // The Config.Bind() format is category name, setting text, default
+            triggerKey = Config.Bind("Hotkeys", "States Activation", new KeyboardShortcut(KeyCode.P, KeyCode.RightControl));
+
+            
+
+            Utility.PostOnMainPage(this.GetType());
+        }
+
+        /// <summary>
+        /// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+            if (Utility.StrictKeyCheck(triggerKey.Value))
+            {
+                if (LocalClient.IsInGmMode)
+                {
+                    if (Utility.isBoardLoaded())
+                    {
+                        CreatureBoardAsset asset;
+                        CreaturePresenter.TryGetAsset(LocalClient.SelectedCreatureId, out asset);
+                        if (asset != null)
+                        {
+                            foreach (PlayerInfo player in CampaignSessionManager.PlayersInfo.Values)
+                            {
+                                UnityEngine.Debug.Log("Probe Plugin: Sending Probe Result To " + player.Name);
+                                StatMessaging.Subscribe(ProbePlugin.Guid + "." + player.Name, HandleRequest);
+                                StatMessaging.SetInfo(LocalClient.SelectedCreatureId, ProbePlugin.Guid + "." + player.Name, "[Probe]");
+                            }
+                        }
+                        else
+                        {
+                            SystemMessage.DisplayInfoText("Probe Requires Selected Mini");
+                        }
+                    }
+                }
+                else
+                {
+                    SystemMessage.DisplayInfoText("Only GM Can Use Probe Function");
+                }
+            }
+        }
+    }
+}
